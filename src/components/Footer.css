3f031ed6c0ba855ab3a7717a675a2e43e6868598
Asker Nagoev <asker.nagoev.02@mail.ru>
2023-02-07 00:50:53 +0300

    Разделил файлы CSS

---
+++ src/components/Footer.css
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <div></div>
+            <div className='foot-text'>
+                © 2023 ФГБОУ ВО "Пятигорский государственный университет"
+            </div>
+            <div></div>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header.css
@@ -0,0 +1,24 @@
+import logoPSU from "./logo.png";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <nav className='head-nav'>
+                <a>Главная</a>
+                <a>Развлечения</a>
+                <a>Творчество</a>
+                <a>Наука</a>
+                <a>Спорт</a>
+                <a>Все</a>
+            </nav>
+
+            <div className='head-space'></div>
+
+            <div className='head-log'>
+                <a>Войти</a>
+                <a>Зарегистрироваться</a>
+            </div>
+        </header>
+    )
+}
+export default Header;
