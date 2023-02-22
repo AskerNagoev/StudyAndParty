f7e6f11f5ebf14f83e6ba0e8e2339f8589bd7789
ToxichQQQ <storozhenko.dev@gmail.com>
2023-02-22 21:57:10 +0300

    Внес небольшие корриектировки

--- src/components/Body/EventsBody/Events/Events.jsx
+++ src/components/Body/EventsBody/Events/Events.jsx
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, {useMemo, useState} from 'react';
 import styles from './Events.module.css';
 import Event from "./Event/Event";
 
-const Events = (props) => {
 
-    let EventsList = props.events.map(event => <Event name={event.name} description={event.description}/>);
+const  institutes = [
+    {label: "Все", value: 'Все'},
+    {label: 'ВШУ', value: 'ВШУ'},
+    {label: 'ИРГЯиГТ', value: 'ИРГЯиГТ'},
+    {label: 'ИМО', value: 'ИМО'},
+    {label: 'ИПРиМ', value: 'ИПРиМ'},
+    {label: 'ВШДиА', value: 'ВШДиА'},
+    {label: 'ЮИ', value: 'ЮИ'},
+    {label: 'ИИЯМТ', value: 'ИИЯМТ'},
+]
+
+
+const Events = (props) => {
+    const [filter, setFilter] = useState( 'Все')
+
+
+    const events = useMemo(() => {
+        return filter === 'Все' ? props.events : props.events.filter(event => event.organizer === filter)
+    },[filter, props])
 
     return (
         <div>
             <div className={styles.top}>
                 <div className={styles.top__search}>
-                    Поиск
-                </div>
-                <div className={styles.top__filters}>
-                    Институт
+                    <input className={styles.search__input} placeholder='Поиск по мероприятиям'/>
+                    <button className={styles.search__button}>Поиск</button>
+                    <select className={styles.search__select} value={filter} onChange={(e) => setFilter(e.currentTarget.value) }>
+                        {institutes.map(institute => <option className={styles.select__option} value={institute.value}>{institute.label}</option>)}
+                    </select>
                 </div>
             </div>
             <div className={styles.events}>
-                {EventsList}
+                {events.length > 0 ? events.map(event =>  <Event key={event.id} name={event.name} description={event.description}/>) :
+                    <div className={styles.empty_screen}>Извините, но мероприятия от {filter} еще не были добавлены в список</div>
+                }
             </div>
         </div>
     )
--- src/components/Body/ProfileBody/ProfileInfo/ProfileInfo.jsx
+++ src/components/Body/ProfileBody/ProfileInfo/ProfileInfo.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, {useRef, useState} from "react";
 import Ava from "./Ava.png";
 import styles from "./ProfileInfo.module.css";
 import Organisation from "./Organisation/Organisation";
 
 const ProfileInfo = (props) => {
+    const [file, setFile] = useState(null);
+    const ref = useRef()
+
+    const handleFileChange = (e) => {
+        if (e.target.files) {
+            setFile(e.target.files[0]);
+        }
+    }
     return (
         <div className={styles.top}>
             <div className={styles.bio}>
                 <div className={styles.bio__info}>
                     <div className={styles.bio__info__photo}>
-                        <img src={Ava} alt="Аватарка"></img>
+                        <img src={file ? URL.createObjectURL(file) : Ava} alt="Аватарка" onClick={() => ref.current.click()}/>
+                        <input type="file" onChange={handleFileChange} ref={ref} />
                     </div>
                     <div className={styles.bio__info__text}>
                         <div>
